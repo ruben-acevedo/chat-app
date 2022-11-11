e08029a85ced203c70cac76a4d5382e2f54e7673
Ruben Acevedo <rubenfsolorzano@hotmail.com>
2022-11-11 18:03:27 -0300

    add socket.io server and index.html

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  io.emit("chat message", "a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    io.emit("chat message", "a user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => console.log("initializing at 3000."));
